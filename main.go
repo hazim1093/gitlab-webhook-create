57c969d352cc4b6d3aa346e0ae77f0aa787cc99f
Hazim Malik <haizm_malik@hotmail.com>
2021-07-04 17:59:16 +0200

    Add check for existing webhooks

--- main.go
+++ main.go
@@ -17,10 +17,25 @@ type Webhook struct {
 	MergeRequestEvents     bool   `json:"mergeRequestEvents"`
 }
 
+func hookExists(client *gitlab.Client, pid int, webhookURL string) bool {
+	projectHooks, _, err := client.Projects.ListProjectHooks(pid, nil)
+	if err != nil {
+		log.Fatalf("Failed to read projects hooks: %v", err)
+	}
+
+	for _, projectHook := range projectHooks {
+		if projectHook.URL == webhookURL {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 
 	webhooksFilePath := os.Getenv("WEBHOOKS_FILE_PATH")
-	gitlabToken := os.Getenv("GITLAB_TOKEN")            
+	gitlabToken := os.Getenv("GITLAB_TOKEN")
 	secretToken := os.Getenv("WEBHOOK_SECRET_TOKEN")
 	enableSslVerification := true
 
@@ -48,6 +63,11 @@ func main() {
 			log.Fatalf("Failed to get Project: %v", err)
 		}
 
+		if hookExists(client, project.ID, webhook.WebhookURL) {
+			log.Printf("Webhook already exists for project '%s' with ID '%d'", webhook.ProjectName, project.ID)
+			continue
+		}
+
 		hook, _, err := client.Projects.AddProjectHook(project.ID, &gitlab.AddProjectHookOptions{
 			URL:                    &webhook.WebhookURL,
 			PushEvents:             &webhook.PushEvents,
@@ -61,6 +81,6 @@ func main() {
 			log.Fatalf("Failed to add Project hook: %v", err)
 		}
 
-		log.Printf("Created webhook: %d", hook.ID)
+		log.Printf("Created webhook: '%d' for project '%s' with ID '%d'", hook.ID, webhook.ProjectName, project.ID)
 	}
 }
